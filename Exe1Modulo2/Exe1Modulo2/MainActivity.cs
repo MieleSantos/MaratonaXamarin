6d4aaa4c524cd09b69f6d66475aea6c6b3e9861d
MieleSantos <mielesnts@gmail.com>
2017-02-08 17:59:18 -0200

    Exemplo 1 do modulo2 da Maratona xamarin

---
+++ Exe1Modulo2/Exe1Modulo2/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Exe1Modulo2
+{
+	[Activity(Label = "Exe1Modulo2", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+			Button btnconverter = FindViewById<Button>(Resource.Id.btnconverter);
+			EditText txtdolares = FindViewById<EditText>(Resource.Id.txtdolares);
+			EditText txtpesos = FindViewById<EditText>(Resource.Id.txtpesos);
+			double pesos, dolares;
+
+			btnconverter.Click += delegate 
+			{
+				try
+				{
+					dolares = double.Parse(txtdolares.Text);
+					pesos = dolares - 19.5;
+					txtpesos.Text = pesos.ToString();
+				}
+				catch (System.Exception ex) 
+				{
+					Toast.MakeText(this, ex.Message, ToastLength.Short).Show();
+				}
+			};
+				
+
+		}
+	}
+}
+
